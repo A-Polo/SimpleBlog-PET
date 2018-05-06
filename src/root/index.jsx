2af742f0cfe2d85b8291a9c6373c702d67979e2f
PolomkinA <polomkinanton@gmail.com>
2018-05-06 13:02:22 +0300

    Update eslint and fix errors

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 const App = () => <div>Hello</div>;
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, global.getElementById('app'));
