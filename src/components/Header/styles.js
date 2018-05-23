1558dd8155b7d9a85eef9681ee487b6e421ea212
PolomkinA <polomkinanton@gmail.com>
2018-05-23 18:39:18 +0300

    Add jss styles for Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import theme from '../../assets/theme/theme';
+
+export default {
+  header: {
+    height: 50,
+    background: theme.headerBackground,
+    padding: [0, 20],
+    lineHeight: '50px',
+  },
+  title: {
+    margin: 0,
+    fontSize: '16px',
+  },
+};
